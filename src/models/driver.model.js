2071ccc7053abafaa4a51d1ac4df92d9bf50bc5c
LuisMunar <luis.emunarr@gmail.com>
2022-05-07 21:41:11 -0500

    Auto increment in driver model is done

--- src/models/driver.model.js
+++ src/models/driver.model.js
@@ -6,7 +6,8 @@ module.exports = (sequelize, Sequelize) => {
     {
       id: {
         type: Sequelize.INTEGER,
-        primaryKey: true
+        primaryKey: true,
+        autoIncrement: true
       },
       company_id: {
         type: Sequelize.INTEGER
